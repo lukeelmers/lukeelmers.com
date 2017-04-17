9700df5a049ebcc2e74164f534fbb4e769283b6c
Luke Elmers <lukeelmers@gmail.com>
2017-04-17 14:44:40 -0600

    add initial styles & particles

--- app/index.js
+++ app/index.js
@@ -1,11 +1,16 @@
 'use strict'
 
+require('particles.js')
+const { particles } = require('./config')
+
+
 class App {
     constructor() {
         if ('addEventListener' in document) {
             document.addEventListener('DOMContentLoaded', () => {
                 document.documentElement.className = 'js'
                 console.log('loaded')
+                particlesJS('particles-js', particles)
             })
         }
     }
